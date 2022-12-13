3115661cc1cae4878684957b81377bee5dc48872
Carlos Soliz <yocarlos666@gmail.com>
2022-12-13 13:12:50 -0400

    demo

--- index.js
+++ index.js
@@ -7,7 +7,19 @@ const app = express();
 app.use(express.static(__dirname + '/www'));
 app.use(cors())
 
-app.get('/*', function(req,res) {
+
+var whitelist = ['http://jycapi-001-site1.atempurl.com']
+var corsOptionsDelegate = function (req, callback) {
+  var corsOptions;
+  if (whitelist.indexOf(req.header('Origin')) !== -1) {
+    corsOptions = { origin: true } // reflect (enable) the requested origin in the CORS response
+  } else {
+    corsOptions = { origin: false } // disable CORS for this request
+  }
+  callback(null, corsOptions) // callback expects two parameters: error and options
+}
+
+app.get('/*',cors(corsOptionsDelegate), function(req,res) {
   res.sendFile(path.join(__dirname+'/www/index.html'));
 });
 
