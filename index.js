21d96f412a423af7ad9e20c822d5029cb647185b
Carlos Soliz <yocarlos666@gmail.com>
2022-12-13 13:00:43 -0400

    demo

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const path = require('path');
 const port = process.env.PORT || 8080;
-
+var cors = require('cors')
 const app = express();
 
 app.use(express.static(__dirname + '/www'));
+app.use(cors())
 
 app.get('/*', function(req,res) {
   res.sendFile(path.join(__dirname+'/www/index.html'));
